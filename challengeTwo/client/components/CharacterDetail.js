47dc060aae1708fa9788708058dfdf5b66a894d2
jkeane889 <jkeane889@gmail.com>
2020-03-11 11:15:02 -0500

    updated dropdown menu

--- challengeTwo/client/components/CharacterDetail.js
+++ challengeTwo/client/components/CharacterDetail.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const CharacterDetail = props => {
+    console.log("These are the props.details: ", props.details)
     return (
         <React.Fragment>
-            <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <Typography display='block'>
-                        Name: {props.details.name ? props.details.name : ''}
-                    </Typography>
-                </Paper>
-            </Grid>
+        <Grid item xs={4}>
+            <Paper>
+                <Typography display='block'>
+                    {props.details[0]}: {props.details[1]}
+                </Typography>
+            </Paper>
+        </Grid>
         </React.Fragment>
-    )
+    );
 }
+
+export default memo(CharacterDetail);
--- challengeTwo/client/components/CharacterInfo.js
+++ challengeTwo/client/components/CharacterInfo.js
@@ -1,105 +1,121 @@
 import axios from 'axios';
-import React, { useState, memo, useEffect } from 'react';
+import React, { memo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import PersonIcon from '@material-ui/icons/Person';
+import HeightIcon from '@material-ui/icons/Height';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import CakeIcon from '@material-ui/icons/Cake';
+import WcIcon from '@material-ui/icons/Wc';
+import LanguageIcon from '@material-ui/icons/Language';
 
 const CharacterInfo = props => {
 
   const useStyles = makeStyles(theme => ({
-      root: {
-        flexGrow: 1,
-      },
-      paper: {
-        padding: theme.spacing(2),
-        marginTop: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      },
+    root: {
+      width: '100%',
+      maxWidth: '100%',
+      marginTop: '2%',
+      backgroundColor: theme.palette.background.paper,
+    },
   }));
   
   if (props.details) {
     const classes = useStyles();
 
-    // Ternary operators to catch if any characteristic is undefined; if so, just returns an empty string
+    console.log("These are the details: ", props.details)
+    let name = props.details.name;
+    let height = props.details.height;
+    let mass = props.details.mass;
+    let hair_color = props.details.hair_color;
+    let skin_color = props.details.skin_color;
+    let eye_color = props.details.eye_color;
+    let birth_year = props.details.birth_year;
+    let gender = props.details.gender;
+    let homeworld = props.details.homeworld;
+
     return (
-      <div className={classes.root}>
-        <Grid container spacing={1}>
-        <React.Fragment>
-        <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Name: {props.details.name ? props.details.name : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-        </React.Fragment>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Name: {props.details.name ? props.details.name : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-              Height: {props.details.height ? props.details.height : '' }
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Mass: {props.details.mass ? props.details.mass : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Hair: {props.details.hair_color ? props.details.hair_color : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Skin: {props.details.skin_color ? props.details.skin_color : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Eye-Color: {props.details.eye_color ? props.details.eye_color : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography display='block'>
-                Birthday: {props.details.birth_year ? props.details.birth_year : ''}
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-                <Typography display='block'>
-                  Gender: {props.details.gender ? props.details.gender : ''}
-                </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-                <Typography display='block'>
-                  Homeworld: {props.details.homeworld ? props.details.homeworld : ''}
-                </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
+      <List className={classes.root}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <PersonIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Name" secondary={name} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <HeightIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Height" secondary={height} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <FitnessCenterIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Mass" secondary={mass} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <InvertColorsIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Hair Color" secondary={hair_color} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <AccessibilityIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Skin Color" secondary={skin_color} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <VisibilityIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Eye Color" secondary={eye_color} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <CakeIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Birthday" secondary={birth_year} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <WcIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Gender" secondary={gender} />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <LanguageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Home World" secondary={homeworld} />
+        </ListItem>
+      </List>
     );
   }
 }
